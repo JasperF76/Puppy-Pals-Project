e1d4f0f6484805adf133499109bb6b7df9d6ce82
JasperF76 <jasjames122@gmail.com>
2024-08-19 14:50:49 -0400

    Introductory React Stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-
-
+  const [puppies, setPuppies] = useState(puppyList)
+console.log(puppies)
   return (
     <>
       <div>
-
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
